dfab97f356cd5b3eb8a21d99b8afad3aca9067ee
Kyaw Khine Htoo <alberthtoo.k@gmail.com>
2024-07-24 21:26:59 +0700

    first commit

--- main.tf
+++ main.tf
@@ -87,15 +87,50 @@ resource "aws_route" "private_rt_route" {
   destination_cidr_block = var.public_internet_destination_cidr
   nat_gateway_id         = aws_nat_gateway.this.id
 }
+resource "tls_private_key" "this" {
+  algorithm = var.private_key_algorithm
+  rsa_bits  = var.private_key_ras_bit
+}
+resource "aws_key_pair" "this" {
+  key_name   = var.aws_key_pair_key_name
+  public_key = tls_private_key.this.public_key_openssh
+}
 
-# # resource "aws_route_table_association" "nat_association" {
-# #   subnet_id      = aws_subnet.public_subnet.id
-# #   route_table_id = aws_route_table.this.id
-# #   gateway_id     = aws_nat_gateway.this.id
-# # }
+resource "aws_security_group" "this" {
+  name        = var.security_group_name
+  description = var.security_group_description
+  vpc_id      = aws_vpc.this.id
 
+}
 
+resource "aws_vpc_security_group_ingress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  from_port         = 22 #port range ဖြစ်တဲ့အတွက် from နှင့် to ထားခြင်းဖြစ်
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
 
-# resource "aws_inst" "name" {
+resource "aws_vpc_security_group_egress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
 
-# }
+resource "aws_instance" "this" {
+  count                       = 1
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.this.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.this.key_name
+  tags = {
+    Name = "${var.instance_name}-${count.index}"
+  }
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.this.private_key_pem
+  sensitive = true
+}
--- variables.tf
+++ variables.tf
@@ -54,3 +54,38 @@ variable "public_internet_destination_cidr" {
   type        = string
   default     = "0.0.0.0/0"
 }
+
+variable "security_group_name" {
+  type        = string
+  description = "Name of AWS Security Group"
+}
+variable "security_group_description" {
+  type        = string
+  description = "Description of AWS Security Group"
+}
+
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID"
+}
+variable "instance_type" {
+  type        = string
+  description = "Instance Type"
+}
+variable "instance_name" {
+  type        = string
+  description = "Instance Name"
+}
+variable "private_key_algorithm" {
+  type        = string
+  description = "Algorithm for Private Key"
+}
+variable "private_key_ras_bit" {
+  type        = string
+  description = "RSA Bit for Private Key"
+}
+variable "aws_key_pair_key_name" {
+  type        = string
+  description = "Key Pair Name for Aws"
+}
